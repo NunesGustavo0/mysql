d987364ba172b61531be1e7e3e30381c21002f0a
Gustavo Nunes <ustivogndo@gmail.com>
2025-04-11 17:15:17 -0400

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,30 @@
+/*
+Triggers
+	Gatilho
+		eventos DML{inserts={new campo}, updates={new, old}, deletes={old}}
+
+Tabela
+	timing{before, after}
+*/
+
+use bd2025;
+
+delimiter $$
+create trigger tg_atualiza_bairro after update on cliente
+for each row
+begin
+	if new.clibaicodigo != old.clibaicodigo then
+		update bairro set beiqtdepessoas = beiqtdepessoas + 1
+		where baicodigo = new.clibaicodigo;
+		update bairro set beiqtdepessoas = beiqtdepessoas + 1
+		where baicodigo = old.clibaicodigo;
+	end if;
+end$$
+delimiter ;
+
+select * from bairro;
+
+update cliente
+set clibaicodigo = 2
+where clicodigo = 1;
+'1', 'ADRIANÓPOLIS', '1', '10549'
